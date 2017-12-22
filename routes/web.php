265f58272be967f1bd7779e67a248dd3659eb8b6
berni <bernardojosellamasverna@gmail.com>
2017-12-22 20:17:01 +0100

    primer video

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function(){
+  echo "<a href=".route('contactos').">Contactos</a><br>";
+  echo "<a href=".route('contactos').">Contactos</a><br>";
+  echo "<a href=".route('contactos').">Contactos</a><br>";
+  echo "<a href=".route('contactos').">Contactos</a><br>";
+  echo "<a href=".route('contactos').">Contactos</a><br>";
 });
+
+Route::get('contacto', ['as' => 'contactos', function(){
+  return "Sección de contactos";
+}]);
+
+//ruta a la que se le envía un parámetro:
+Route::get('saludos/{nombre?}', function($nombre = "Magufo"){
+  return "Hola desde saludos $nombre";
+})->where('nombre', "[A-Za-z]+");
