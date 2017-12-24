4f7213322d5a8330d61d3a553cd1a458ee84ae32
berni <bernardojosellamasverna@gmail.com>
2017-12-24 14:22:08 +0100

    video 19, estilos

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,16 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <style>
-      .active{
-        text-decoration: none;
-        color: green;
-      }
-      .error{
-        color:red;
-        font-size:12px;
-      }
-    </style>
+    <link rel="stylesheet" href="/css/app.css">
     <title>Mi sitio</title>
   </head>
   <body>
@@ -23,33 +14,68 @@
           return request()->is($url) ? 'active':'';
         }
       ?>
-      <nav>
-        {{--Inicio--}}
-        <a class="{{ activeMenu('/') }}"
-           href="{{route('home')}}">Inicio</a>
-        {{--Saludos--}}
-        <a class="{{ activeMenu('saludos*') }}"
-           href="{{route('saludos', 'BerniPollas')}}">Saludos</a>
-        {{--Contactos--}}
-        <a class="{{ activeMenu('mensajes/create') }}"
-           href="{{route('mensajes.create')}}">Contactos</a>
-        {{--Logout:checkeamos antes si hay un usuario autenticado--}}
-        @if (auth()->check())
-          {{--Mensajes--}}
-          <a class="{{ activeMenu('mensajes') }}"
-             href="{{route('mensajes.index')}}">Mensajes</a>
-          {{--Cerrar sesión--}}
-          <a href="/logout">Cerrar sesión {{auth()->user()->name}}</a>
-        @endif
-        {{--Login--}}
-        {{--evaluamos quién se inscribe en la app--}}
-        @if (auth()->guest())
-          <a class="{{ activeMenu('login') }}" href="/login">Login</a>
-        @endif
-      </nav>
+      <nav class="navbar navbar-default" role="navigation">
+	       <div class="container">
+		         <!-- Brand and toggle get grouped for better mobile display -->
+		      <div class="navbar-header">
+			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				     <span class="sr-only">Toggle navigation</span>
+				     <span class="icon-bar"></span>
+				     <span class="icon-bar"></span>
+				     <span class="icon-bar"></span>
+			      </button>
+			       <a class="navbar-brand" href="${1:#}">Title</a>
+		     </div>
+		     <!-- Collect the nav links, forms, and other content for toggling -->
+		     <div class="collapse navbar-collapse navbar-ex1-collapse">
+			       <ul class="nav navbar-nav">
+               <!--Inicio-->
+               <li class="{{ activeMenu('/') }}">
+                 <a href="{{ route('home') }}">Inicio</a>
+               </li>
+               <!--Saludo-->
+               <li class="{{ activeMenu('saludos*') }}">
+                 <a href="{{ route('saludos', 'Ber') }}">Saludo</a>
+               </li>
+               <!--Contacto-->
+               <li class="{{ activeMenu('mensajes/create') }}">
+                 <a href="{{ route('mensajes.create') }}">Contacto</a>
+               </li>
+               <!--Mensajes-->
+               @if(auth()->check())
+                 <li class="{{ activeMenu('mensajes*') }}">
+                   <a href="{{ route('mensajes.index') }}">Mensajes</a>
+                 </li>
+               @endif
+			       </ul>
+			       <ul class="nav navbar-nav navbar-right">
+               <!--Login-->
+               @if(auth()->guest())
+                 <li class="{{ activeMenu('login') }}">
+                   <a href="/login">Login</a>
+                 </li>
+               @else
+                 <li>
+                   <a href="/logout">Cerrar sesión de {{ auth()->user()->name}}</a>
+                 </li>
+               @endif
+				        <li class="dropdown">
+					          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+					          <ul class="dropdown-menu">
+						          <li><a href="#">Action</a></li>
+						          <li><a href="#">Another action</a></li>
+						          <li><a href="#">Something else here</a></li>
+						          <li><a href="#">Separated link</a></li>
+					          </ul>
+				       </li>
+			       </ul>
+		      </div><!-- /.navbar-collapse -->
+	       </div>
+       </nav>
     </header>
-
-    @yield('contenido')
-    <footer>Copyright {{date('Y')}}</footer>
+    <div class="container">
+      @yield('contenido')
+      <footer>Copyright {{date('Y')}}</footer>
+    </div>
   </body>
 </html>
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -4,7 +4,7 @@
   <h1>Todos los mensajes</h1>
 
   {{--tabla para mostrar los mensajes--}}
-  <table width="100%" border="1">
+  <table class="table">
     <thead>
       <tr>
         <th>ID</th>
@@ -27,11 +27,11 @@
           <td>{{ $message->email }}</td>
           <td>{{ $message->mensaje }}</td>
           <td>
-            <a href="{{ route('mensajes.edit', $message->id) }}">Editar</a>
+            <a class="btn btn-info btn-xs" href="{{ route('mensajes.edit', $message->id) }}">Editar</a>
             <form style="display:inline" method="post" action="{{ route('mensajes.destroy', $message->id) }}">
               {!! csrf_field() !!}
               {!! method_field('DELETE') !!}
-              <button type="submit">Eliminar</button>
+              <button class="btn btn-danger btn-xs" type="submit">Eliminar</button>
             </form>
           </td>
         </tr>
