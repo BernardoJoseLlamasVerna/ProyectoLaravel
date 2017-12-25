da259a6a391f1b9011152063b66a74c4c56a50f2
berni <bernardojosellamasverna@gmail.com>
2017-12-25 20:28:44 +0100

    video 20, funciona dropdown

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -77,6 +77,6 @@
       @yield('contenido')
       <footer>Copyright {{date('Y')}}</footer>
     </div>
-    <script type="/js/all.js"></script>
+    <script src="/js/all.js"></script>
   </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,39 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.scripts([
+     'node_modules/jquery/dist/jquery.js',
+     'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+     'resources/assets/js/app.js'
+], 'public/js/all.js', './');
+
+mix.browserSync({
+  proxy:'http://127.0.0.1:8000'
+});
+
+/*mix.scripts([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js'
+], 'public/js/all.js');*/
+
+/*mix.scripts([
   'jquery/dist/jquery.js',
   'bootstrap-sass/assets/javascripts/bootstrap.js'
-], 'public/js/all.js', 'node_modules');
+], 'public/js/all.js', 'node_modules');*/
+
+/*let mix = require('laravel-mix');
+
+//tareas de laravel-mix:
+//
+//0.-tarea por defecto:sass
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.scripts([
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
+  'resources/assets/js/app.js'
+], 'public/js/all.js', './');
+
+mix.browserSync({
+  proxy:'http://127.0.0.1:8000'
+});*/
